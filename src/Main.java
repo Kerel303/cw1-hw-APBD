0860c741598855150363d34ead0c3b8ff7821491
Karol Foltyn <s33765@pjwstk.edu.pl>
2026-03-12 13:06:51 +0100

    Text added

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj rozmiar panelu: ");
         int size = scanner.nextInt();
+
 
         for(int i = 0; i < size; i++){
             for(int j = 0; j < size; j++){
@@ -11,7 +13,6 @@ public class Main {
             }
             System.out.println();
         }
-
 
     }
 }

c82713ac80378f9f534414470dc5e487179910bf
Karol Foltyn <s33765@pjwstk.edu.pl>
2026-03-12 13:10:42 +0100

    Adding chances to create something different

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,23 @@ public class Main {
         System.out.println("Podaj rozmiar panelu: ");
         int size = scanner.nextInt();
 
+        System.out.println("Podaj szanse wystapienia innego elementu (w %): ");
+        double chance = scanner.nextDouble();
+        if (chance > 100) {
+            chance = 100;
+        }
+        if (chance < 0) {
+            chance = 0;
+        }
+        chance = chance/100;
 
         for(int i = 0; i < size; i++){
             for(int j = 0; j < size; j++){
-                System.out.print(" * ");
+                if(Math.random() > chance){
+                    System.out.print(" * ");
+                }else {
+                    System.out.print(" # ");
+                }
             }
             System.out.println();
         }
